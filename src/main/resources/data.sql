146e1519f04d339ea301853670576b62380f92c9
silkwave <silkwave24@gmail.com>
2026-01-09 16:22:13 +0900

    roundrobin

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO servers (name, url, active) VALUES ('Server A', 'https://jsonplaceholder.typicode.com/posts', true);
 INSERT INTO servers (name, url, active) VALUES ('Server B', 'https://jsonplaceholder.typicode.com/posts/1', true);
 INSERT INTO servers (name, url, active) VALUES ('Server C', 'https://jsonplaceholder.typicode.com/posts/1/comments', true);
+INSERT INTO servers (name, url, active) VALUES ('Server D', 'https://jsonplaceholder.typicode.com/posts/1/comments', true);
